c39a643a7c6318abde894b3487d6cc834d2d4f9b
Qiao Jiang <qiaojiang@qiaos-mbp.myfiosgateway.com>
2022-04-06 15:21:24 -0400

    initial commit

---
+++ contracts/test/MockWETH.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract MockWETH is ERC20 {
+    constructor() public ERC20("Mock WETH", "WETH") {}
+}
---
+++ tests/unit/test_token_farm.py
@@ -0,0 +1,71 @@
+from webbrowser import get
+import pytest
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    INITIAL_VALUE,
+    get_account,
+    get_contract,
+)
+from brownie import network, config, exceptions
+import pytest
+from scripts.deploy import deploy_token_farm_and_dapp_token
+
+
+def test_set_price_feed_contract():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing!")
+    account = get_account()
+    non_owner = get_account(index=1)
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+
+    # Act
+    price_feed_address = get_contract("eth_usd_price_feed")
+    token_farm.setPriceFeedContract(
+        dapp_token.address, get_contract("eth_usd_price_feed"), {"from": account}
+    )
+
+    # Assert
+    assert token_farm.tokenPriceFeedMapping(dapp_token.address) == price_feed_address
+    with pytest.raises(exceptions.VirtualMachineError):
+        token_farm.setPriceFeedContract(
+            dapp_token.address, get_contract("eth_usd_price_feed"), {"from": non_owner}
+        )
+
+
+def test_stake_tokens(amount_staked):
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing!")
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # Act
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, dapp_token.address, {"from": account})
+
+    # Assert
+    assert (
+        token_farm.stakingBalance(dapp_token.address, account.address) == amount_staked
+    )
+
+    assert token_farm.uniqueTokensStaked(account.address) == 1
+    assert token_farm.stakers(0) == account.address
+    return token_farm, dapp_token
+
+
+def test_issue_tokens(amount_staked):
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing!")
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    token_farm, dapp_token = test_stake_tokens(amount_staked)
+
+    starting_balance = dapp_token.balanceOf(account.address)
+    # Act
+    token_farm.issueTokens({"from": account})
+    # Arrange
+    # We are staking one dapp token == in price to 1 ETH
+    # so... we should get 2,000 dapp tokens in reward
+    # Since the price of ETH is $2,000
+    assert dapp_token.balanceOf(account.address) == starting_balance + INITIAL_VALUE
